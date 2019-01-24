ccf352b80f78dc84430049be0a5b46727d255a87
Alexander Baryshnikov <dev@baryshnikov.net>
2019-01-25 01:16:50 +0800

    add core class for knowledge

---
+++ knowledge.py
@@ -0,0 +1,129 @@
+import re
+from typing import Tuple, List, Union, Optional, Pattern
+
+
+async def Handler(message: str, **params: str) -> Optional[Union[str, bytes]]:
+    """
+    Example of handler type
+    :param message: message text without trigger word
+    :param params: named groups for regexp-based matching
+    :return: None if nothing matched, string or bytes
+    """
+    pass  # typedef
+
+
+class Knowledge:
+    """
+    Base async router for incoming messages.
+    Routes messages if message starts from one of trigger word.
+    Router's can be nested as usual triggers.
+    Handlers may throw an exception that will be printed and suppressed (except KeyboardException).
+    If no triggers specified - any message will be accepted
+
+    @:param triggers: case-insensitive words that will be used as a trigger
+    """
+
+    def __init__(self, *triggers: str):
+        self.__handlers = []  # type: List[Tuple[Pattern,Handler],...]
+        self.__default = None  # type: Optional[Handler]
+        self.__triggers = tuple(x.lower() for x in triggers)
+
+    async def __call__(self, message_text: str) -> Optional[Union[str, bytes]]:
+        """
+        Check that message contains trigger word in beginning (case-insensitive) and tries to check all handlers one-by-one.
+        If handler's pattern matched and callback returns not None, result is returned as is.
+        If no successful handler found, default handler used (if defined)
+        If no triggers specified - any message will be accepted
+        :param message_text: any text message
+        :return: None - if nothing matched or if default handler returned not, otherwise expects string or bytes
+        """
+        triggered = len(self.__triggers) == 0
+        for trigger in self.__triggers:
+            if message_text.lower().startswith(trigger):
+                message_text = message_text[len(trigger):]
+                triggered = True
+                break
+
+        if not triggered:
+            return None
+
+        for (pattern, handler) in self.__handlers:
+            match = pattern.match(message_text)
+            if match:
+                reply = None
+                try:
+                    reply = await handler(message_text, **match.groupdict())
+                except KeyboardInterrupt:
+                    # it's a program termination
+                    return None
+                except Exception as ex:
+                    print("error on processing", message_text, ":", ex)
+                if reply is not None:
+                    return reply
+        if self.__default is not None:
+            return await self.__default(message_text)
+
+    def simple(self, keyword: str, handler: Handler = None):
+        """
+        Add simple non-regexp handler that matched if text exact as in message
+        :param keyword: trigger text
+        :param handler: standard handler or none for annotation wrapper
+        """
+        pattern = re.compile(re.escape(keyword))
+        if handler is None:
+            return self.__register(pattern)
+        self.__handlers.append((pattern, handler))
+
+    def prefix(self, prefix: str, handler: Handler = None):
+        """
+        Add prefix-based handler that matched if text has specified prefix. Handler got message without prefix
+        :param prefix: trigger prefix
+        :param handler: standard handler or none for annotation wrapper that should expect message without prefix
+        """
+        pattern = re.compile(f'^{re.escape(prefix)}.*?')
+        if handler is None:
+            def wrapper(f):
+                async def without_prefix(message: str):
+                    return await f(message[len(prefix):])
+
+                self.__handlers.append((pattern, without_prefix))
+                return f
+
+            return wrapper
+
+        async def without_prefix(message: str):
+            return await handler(message[len(prefix):])
+
+        self.__handlers.append((pattern, without_prefix))
+
+    def regexp(self, pattern_text: str, handler: Handler = None):
+        """
+        Add regexp handler that matched if pattern matched message.
+        Named groups will be used as named arguments in handler
+        :param pattern_text: any valid regexp
+        :param handler: standard handler or none for annotation wrapper. should expects named arguments if pattern contains named groups
+        """
+        pattern = re.compile(pattern_text)
+        if handler is None:
+            return self.__register(pattern)
+        self.__handlers.append((pattern, handler))
+
+    def default(self, handler: Handler = None):
+        """
+        Set default handler for non-matched messages
+        :param handler: standard handler or none for annotation wrapper
+        """
+        if handler is None:
+            def wrapper(f):
+                self.__default = f
+                return f
+
+            return wrapper
+        self.__default = handler
+
+    def __register(self, pattern: Pattern):
+        def wrapper(f):
+            self.__handlers.append((pattern, f))
+            return f
+
+        return wrapper
---
+++ test_knowledge.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+import asyncio
+from knowledge import Knowledge
+
+
+class TestKnowledge(TestCase):
+    def test_annotations(self):
+        brain = Knowledge('alice ', 'bob ')
+
+        @brain.simple('what time is it now?')
+        async def current_time(message: str):
+            return 'no time - no problem'
+
+        @brain.regexp(r'I am (?P<name>\w+)')
+        async def greet(message: str, name: str):
+            return f"Hello, {name}!"
+
+        @brain.simple("raise an error")
+        async def broken(message: str):
+            raise RuntimeError("test error")
+
+        @brain.simple("skip none")
+        async def skip_none(message: str):
+            return None
+
+        @brain.default()
+        async def unknown(message: str):
+            return "WTF?"
+
+        async def main():
+            assert await brain('bob what time is it now?') == 'no time - no problem', 'simple match'
+            assert await brain('alice I am Brayan!') == 'Hello, Brayan!', 'regexp named match'
+            assert await brain('nick I am Alice') is None, 'no trigger'
+            assert await brain('alice raise an error') == 'WTF?', 'suppress exception'
+            assert await brain('alice skip none') == 'WTF?', 'skip none result'
+            assert await brain('bob who are you?') == 'WTF?', 'default trigger'
+
+        asyncio.get_event_loop().run_until_complete(main())
+
+    def test_nested(self):
+        alice = Knowledge('alice ')
+        bob = Knowledge()
+
+        @bob.simple("about time")
+        async def touch(message: str):
+            assert message == 'about time'
+            return 'time is nothing'
+
+        alice.prefix('ask bob ', bob)  # make nested knowledge
+
+        reply = asyncio.get_event_loop().run_until_complete(alice('alice ask bob about time'))
+        print("reply", reply)
+        assert reply == 'time is nothing', 'nested knowledge'
